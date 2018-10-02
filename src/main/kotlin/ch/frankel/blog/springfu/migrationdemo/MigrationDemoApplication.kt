4e242f3d9560ee67a66e023f7068ea2a68efcfd0
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-10-02 09:26:20 +0200

    Migrate to Kofu 0.0.2

--- src/main/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplication.kt
+++ src/main/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplication.kt
@@ -1,31 +1,42 @@
 package ch.frankel.blog.springfu.migrationdemo
 
-import org.springframework.boot.*
-import org.springframework.boot.autoconfigure.*
-import org.springframework.context.support.*
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.boot.kofu.application
+import org.springframework.boot.kofu.mongo.embedded
+import org.springframework.boot.kofu.mongo.mongodb
+import org.springframework.boot.kofu.ref
+import org.springframework.boot.kofu.web.jackson
+import org.springframework.boot.kofu.web.server
+import org.springframework.context.support.beans
 import org.springframework.data.annotation.*
+import org.springframework.data.mongodb.core.*
 import org.springframework.data.mongodb.core.mapping.*
-import org.springframework.data.mongodb.repository.*
 import org.springframework.web.reactive.function.server.*
-import org.springframework.web.server.adapter.*
 import java.time.Duration
 import java.time.LocalDate
 
-@SpringBootApplication
-class MigrationDemoApplication
+val beans = beans {
+    bean<PersonHandler>()
+    bean<PersonRepository>()
+}
 
-fun beans() = beans {
-    bean {
-        CommandLineRunner {
-            ref<PersonRepository>().insert(
-                    arrayListOf(Person(1, "John", "Doe", LocalDate.of(1970, 1, 1)),
-                                Person(2, "Jane", "Doe", LocalDate.of(1970, 1, 1)),
-                                Person(3, "Brian", "Goetz"))
-            ).blockLast(Duration.ofSeconds(2))
+val app = application {
+    import(beans)
+    listener<ApplicationReadyEvent> {
+        ref<PersonRepository>().insert(
+                arrayListOf(Person(1, "John", "Doe", LocalDate.of(1970, 1, 1)),
+                        Person(2, "Jane", "Doe", LocalDate.of(1970, 1, 1)),
+                        Person(3, "Brian", "Goetz"))
+        ).blockLast(Duration.ofSeconds(2))
+    }
+    server {
+        import(::routes)
+        codecs {
+            jackson()
         }
     }
-    bean {
-        routes(PersonHandler(ref()))
+    mongodb {
+        embedded()
     }
 }
 
@@ -42,12 +53,14 @@ class PersonHandler(private val personRepository: PersonRepository) {
 }
 
 fun main(args: Array<String>) {
-    runApplication<MigrationDemoApplication>(*args) {
-        addInitializers(beans())
-    }
+    app.run(args)
 }
 
 @Document
 class Person(@Id val id: Long, val firstName: String, val lastName: String, val birthdate: LocalDate? = null)
 
-interface PersonRepository : ReactiveMongoRepository<Person, Long>
+class PersonRepository(private val mongo: ReactiveMongoOperations) {
+    fun findAll() = mongo.findAll<Person>()
+    fun findById(id: Long) = mongo.findById<Person>(id)
+    fun insert(persons: List<Person>) = mongo.insert(persons, Person::class)
+}
--- src/test/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplicationTests.kt
+++ src/test/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplicationTests.kt
@@ -1,16 +1,12 @@
 package ch.frankel.blog.springfu.migrationdemo
 
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
 class MigrationDemoApplicationTests {
 
     @Test
     fun contextLoads() {
+        app.run()
     }
 
 }
