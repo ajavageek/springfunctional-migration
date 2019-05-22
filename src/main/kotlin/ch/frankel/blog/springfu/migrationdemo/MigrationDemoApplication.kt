b24d4ac75758fbd6d2b40bfc4364a26f1cff21f0
Nicolas Frankel <nicolas@frankel.ch>
2019-05-22 16:16:59 +0200

    Migrate from annotation-based configuration to explicit initialization

--- src/main/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplication.kt
+++ src/main/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplication.kt
@@ -2,32 +2,32 @@ package ch.frankel.blog.springfu.migrationdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.context.support.beans
 import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.function.*
-import org.springframework.web.servlet.function.RequestPredicates.*
-import org.springframework.web.servlet.function.RouterFunctions.nest
-import org.springframework.web.servlet.function.RouterFunctions.route
 import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @SpringBootApplication
-class MigrationDemoApplication {
+class MigrationDemoApplication
 
-    @Bean
-    fun routes(repository: PersonRepository) = router {
-        val handler = PersonHandler(repository)
-        "/person".nest {
-            GET("/{id}", handler::readOne)
-            GET("/") { handler.readAll() }
+fun beans() = beans {
+    bean {
+        router {
+            "/person".nest {
+                val handler = PersonHandler(ref())
+                GET("/{id}", handler::readOne)
+                GET("/") { handler.readAll() }
+            }
         }
     }
 }
 
 fun main(args: Array<String>) {
-    runApplication<MigrationDemoApplication>(*args)
+    runApplication<MigrationDemoApplication>(*args) {
+        addInitializers(beans())
+    }
 }
 
 class PersonHandler(private val personRepository: PersonRepository) {
