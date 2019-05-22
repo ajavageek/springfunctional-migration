f284550fe7cf4177eeb814a1405594165df19646
Nicolas Frankel <nicolas@frankel.ch>
2019-05-22 15:44:44 +0200

    Migrate controllers to router functions

--- src/main/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplication.kt
+++ src/main/kotlin/ch/frankel/blog/springfu/migrationdemo/MigrationDemoApplication.kt
@@ -2,27 +2,35 @@ package ch.frankel.blog.springfu.migrationdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.function.HandlerFunction
+import org.springframework.web.servlet.function.RequestPredicates.*
+import org.springframework.web.servlet.function.RouterFunctions.nest
+import org.springframework.web.servlet.function.RouterFunctions.route
+import org.springframework.web.servlet.function.ServerResponse
 import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @SpringBootApplication
-class MigrationDemoApplication
+class MigrationDemoApplication {
+
+    @Bean
+    fun routes(repository: PersonRepository) = nest(
+        path("/person"),
+        route(
+            GET("/{id}"),
+            HandlerFunction { ServerResponse.ok().body(repository.findById(it.pathVariable("id").toLong())) })
+            .andRoute(
+                method(HttpMethod.GET),
+                HandlerFunction { ServerResponse.ok().body(repository.findAll()) })
+    )
+}
 
 fun main(args: Array<String>) {
     runApplication<MigrationDemoApplication>(*args)
-}
-
-@RestController
-class PersonController(private val personRepository: PersonRepository) {
-
-    @GetMapping("/person")
-    fun readAll() = personRepository.findAll()
-
-    @GetMapping("/person/{id}")
-    fun readOne(@PathVariable id: Long) = personRepository.findById(id)
 }
 
 @Entity
